798f871dbece3d8d7639047e5fd4bb8cd4520b7b
Eligio93 <eligio.cristantielli@gmail.com>
2023-05-02 17:25:02 +1000

    add placeholders back when input is empty

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
+// This replace the placeholder in case the input loose focus with no data
 const emptyField=document.querySelectorAll("input").forEach(function(e){
     e.addEventListener("click", function(){
+        let placeholder= e.placeholder;
+        console.log(placeholder);
         e.setAttribute("placeholder", "");
+        e.addEventListener("blur",function(){
+            if(e.value===""){
+                e.setAttribute("placeholder", placeholder);
+            }
+        })
     })
 });
